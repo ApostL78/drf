3d1e9c41acff147dc669a8cc2b26987be8a45a23
ApostL78 <vgwrbrvw@mail.ru>
2022-11-16 18:42:21 +0400

    [add] RawPersonAPIView [upd] urls.py

--- famous_persons/urls.py
+++ famous_persons/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from famous_persons import views
 
 urlpatterns = [
-    path('personlist/', views.PersonAPIView.as_view(), name='person-list'),
+    path("person-list/", views.PersonAPIView.as_view(), name="person-list"),
+    path("raw-person-list/", views.RawPersonAPIView.as_view(), name="raw-person-list"),
 ]
--- famous_persons/views.py
+++ famous_persons/views.py
@@ -1,7 +1,24 @@
+from django.forms import model_to_dict
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from famous_persons.models import Person
 from famous_persons.serializers import PersonSerializer
+
+
+class RawPersonAPIView(APIView):
+    def get(self, request):
+        persons = Person.objects.all().values()
+        return Response({"posts": list(persons)})
+
+    def post(self, request):
+        new_person = Person.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            role=request.data['role_id']
+        )
+        return Response({'post': model_to_dict(new_person)})
 
 
 class PersonAPIView(generics.ListAPIView):
