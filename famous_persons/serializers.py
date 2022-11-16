965d143aba6904770727f4d595ddad71de9f323d
ApostL78 <vgwrbrvw@mail.ru>
2022-11-16 19:34:05 +0400

    [add] raw custom serializer PersonSerializer; usage of custom serializer in RawPersonAPIView

--- famous_persons/serializers.py
+++ famous_persons/serializers.py
@@ -3,8 +3,16 @@ from rest_framework import serializers
 from famous_persons.models import Person
 
 
-class PersonSerializer(serializers.ModelSerializer):
-    
+class PersonSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    role_id = serializers.IntegerField()
+
+
+class ModelPersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
         fields = ('title', 'role_id')
--- famous_persons/views.py
+++ famous_persons/views.py
@@ -1,26 +1,28 @@
-from django.forms import model_to_dict
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from famous_persons.models import Person
-from famous_persons.serializers import PersonSerializer
+from famous_persons.serializers import ModelPersonSerializer, PersonSerializer
 
 
 class RawPersonAPIView(APIView):
     def get(self, request):
-        persons = Person.objects.all().values()
-        return Response({"posts": list(persons)})
+        persons = Person.objects.all()
+        return Response({"posts": PersonSerializer(persons, many=True).data})
 
     def post(self, request):
+        serializer = PersonSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         new_person = Person.objects.create(
             title=request.data['title'],
             content=request.data['content'],
             role=request.data['role_id']
         )
-        return Response({'post': model_to_dict(new_person)})
+        return Response({'post': PersonSerializer(new_person).data})
 
 
 class PersonAPIView(generics.ListAPIView):
     queryset = Person.objects.all()
-    serializer_class = PersonSerializer
+    serializer_class = ModelPersonSerializer
