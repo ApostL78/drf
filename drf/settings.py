af3537a388a991d31ec83aaf7e2c44d59709fd76
ApostL78 <vgwrbrvw@mail.ru>
2023-01-01 16:00:00 +0000

    [add] Session-based auth

--- drf/settings.py
+++ drf/settings.py
@@ -125,8 +125,12 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
-    ]
+    ],
 }
--- drf/urls.py
+++ drf/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('famous_persons.urls')),
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("famous_persons.urls")),
+    path("api/v1/drf-auth/", include("rest_framework.urls")),
 ]
